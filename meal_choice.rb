9f3abe955f4a73b193bd5596e90718bd5227dd02
David Zeit <dzeit216@gmail.com>
2018-04-11 18:38:58 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,11 @@
-def meal_choice(type_of_meal=meat)
-  puts meal_choice
+def meal_choice(type_of_meal = meat)
+  puts "Today's meal is #{type_of_meal}."
 end
 
+meal_choice()
+
 def meal_choice(type_of_meal)
-  puts meal_choice
+  puts "Today's meal is #{type_of_meal}."
 end
+
+meal_choice("vegan")
